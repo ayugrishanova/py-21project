bec12c0486ba33ad81f3e1e6508b13d767185c0b
nastyafirs <56596762+nastyafirs@users.noreply.github.com>
2022-06-02 16:12:19 +0300

    Update reviews_wordcloud.py

--- sentimental_analysis/reviews_wordcloud.py
+++ sentimental_analysis/reviews_wordcloud.py
@@ -1,3 +1,54 @@
+!tar xvzf aclImdb_v1.tar.gz
+
+import os
+
+def fetch_reviews(path):
+  data = []
+  files = [f for f in os.listdir(path)]
+  for file in files:
+    with open(path+file, "r", encoding='utf8') as f:
+      data.append(f.read()) 
+  return data
+
+import pandas as pd
+
+df_train_pos = pd.DataFrame({'review': fetch_reviews('aclImdb/train/pos/'), 'label': 'pos'})
+df_train_neg = pd.DataFrame({'review': fetch_reviews('aclImdb/train/neg/'), 'label': 'neg'})
+df_test_pos = pd.DataFrame({'review': fetch_reviews('aclImdb/test/pos/'), 'label': 'pos'})
+df_test_neg = pd.DataFrame({'review': fetch_reviews('aclImdb/test/neg/'), 'label': 'neg'})
+
+df = pd.concat([df_train_pos, df_train_neg, df_test_pos, df_test_neg], ignore_index=True)
+
+import re
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+stop_words = stopwords.words('english') 
+stop_words.remove('not') 
+lemmatizer = WordNetLemmatizer()
+
+def data_preprocessing(review):
+    review = re.sub(re.compile('<.*?>'), '', review) 
+    review =  re.sub('[^A-Za-z0-9]+', ' ', review)
+    review = review.lower()
+    tokens = nltk.word_tokenize(review)
+    review = [word for word in tokens if word not in stop_words]
+    review = [lemmatizer.lemmatize(word) for word in review]
+    review = ' '.join(review)
+    return review
+ 
+df['preprocessed_review'] = df['review'].apply(lambda review: data_preprocessing(review))
+words_list_pos = df[df['label']=='pos']['preprocessed_review'].unique().tolist()
+pos_words = " ".join(words_list_pos)
+words_list_neg = df[df['label']=='neg']['preprocessed_review'].unique().tolist()
+neg_words = " ".join(words_list_neg)
+
+
 #pip install wordcloud 
 #conda install -c conda-forge wordcloud
 
