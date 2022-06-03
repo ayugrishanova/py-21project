f281921fea63d188fa6f9550408a48c8277372c8
LizaDerb <90270137+LizaDerb@users.noreply.github.com>
2022-06-04 01:17:44 +0300

    Update for_one_article_with_html.py

--- предварительная обработка данных/for_one_article_with_html.py
+++ предварительная обработка данных/for_one_article_with_html.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import string
 import nltk
 
+# удаление html-тэгов
+
 file = open('test.txt', "r", encoding="utf-8")
 contents = file.read()
 soup = BeautifulSoup(contents, 'lxml')
@@ -10,6 +12,8 @@ html_free = soup.get_text('\n', strip='True')
 
 text = html_free.lower()
 
+# удаление пунктуации и прочих лишних симвлов
+
 cleaning = string.punctuation + "\n\xa0«»\t—…" + string.digits + "'‘’"
 new_text = ''
 
@@ -17,9 +21,13 @@ for ch in text:
     if ch not in cleaning:
         new_text += ch
 
+# токенизация
+        
 from nltk import word_tokenize
 #nltk.download('punkt')
 text_tokens = word_tokenize(new_text)
+
+# удаление стоп-слов
 
 from nltk.corpus import stopwords
 #nltk.download('stopwords')
@@ -33,7 +41,8 @@ for w in text_tokens:
     if w not in english_stopwords:
         no_stopwords = no_stopwords + w + ' '
 
-
+# лемматизация и формирование списка частотных лемм
+        
 #pip install -U pip setuptools wheel
 #pip install -U spacy
 #python -m spacy download en_core_web_sm
@@ -54,6 +63,8 @@ from nltk.probability import FreqDist
 fdist = FreqDist(text_lemma)
 
 print(fdist.most_common(50))
+
+#создание облака частотных слов с помощью matplotlib
 
 #pip install worldcloud
 from wordcloud import WordCloud
